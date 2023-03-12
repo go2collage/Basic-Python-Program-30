58f6617d9ecb53b3a46176ec26f2077948dd7bea
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-12 14:48:36 +0530

    Add files via upload

---
+++ BP_P30.py
@@ -0,0 +1,14 @@
+# Find repeated items of a Tuple
+
+# create tuple
+tuple1 = (1, 3, 4, 6, 5, 4, 2, 7, 9, 10, 7, 4, 4)
+
+print("Tuple is: ", tuple1)
+
+count1 = tuple1.count(7)         # count() function used to count a number repeated no. 
+print("7 repeated: ", count1)
+
+count2 = tuple1.count(4)
+print("4 repeated: ", count2)
+
+# Thanks for Watching
